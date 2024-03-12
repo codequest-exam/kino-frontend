f58cfbe5598bd23d8d965772ddc857de7ccb2745
Jaes98 <marcus.jaes@gmail.com>
2024-03-12 11:59:15 +0100

    temp home page updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
+import React from 'react';
+
 export default function Home() {
     return (
+      <div>
+        <img style={{ width: 700 }} src="./logo.png" alt="logo" />
         <div>
-        <h2>Welcome to the home page</h2>
-        <p>
-            This is an application to demonstrate how to build a movie ticket booking system using React and TypeScript.
-        </p>
+          <h2>Welcome to the home page</h2>
+          <p>
+            This is an application to demonstrate how to build a movie ticket
+            booking system using React and TypeScript.
+          </p>
         </div>
+        <div
+          style={{
+            flex: 1,
+            backgroundImage: "url(./logo.png)",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+      </div>
     );
-    }
+}
