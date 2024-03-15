ecc549a9790b98910a79aa8369c3a0d9375c176f
LauritsSchou <laurits.schou@gmail.com>
2024-03-15 11:48:11 +0100

    changed apiFacade to use API_URL from settings

--- src/services/apiFacade.ts
+++ src/services/apiFacade.ts
@@ -1,41 +1,39 @@
-// import { API_URL } from "../settings";
+import { API_URL } from "../settings";
 import { handleHttpErrors } from "../services/fetchUtils";
 
 const CACHE_TIME = 1 * 60 * 1000; // 1 min cache
-// const MOVIE_URL = API_URL + "/movies";
+const MOVIE_URL = API_URL + "/movies";
 const LAST_FETCH = { movies: 0 };
-const LOCAL_HOST_URL = "http://localhost:8080/";
 
 interface Movie {
-        id: number;
-        title: string;
-        year: string;
-        rated: string;
-        runtime: string;
-        genre: string;
-        director: string;
-        writer: string;
-        actors: string;
-        metascore: string;
-        imdbRating: string;
-        imdbVotes: string;
-        website: string;
-        response: string;
-        plot: string;
-        poster: string;
-        imdbID: string;
+  id: number;
+  title: string;
+  year: string;
+  rated: string;
+  runtime: string;
+  genre: string;
+  director: string;
+  writer: string;
+  actors: string;
+  metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  website: string;
+  response: string;
+  plot: string;
+  poster: string;
+  imdbID: string;
 }
 
 let movies: Array<Movie> = [];
 
-
 async function getMovie(id: number): Promise<Movie> {
-    return fetch(LOCAL_HOST_URL + "movie" + "/" + id).then(handleHttpErrors);
+  return fetch(MOVIE_URL + "/" + id).then(handleHttpErrors);
 }
 
 async function getMovies(): Promise<Array<Movie>> {
   if (LAST_FETCH.movies > Date.now() - CACHE_TIME) return [...movies];
-  const res = await fetch(LOCAL_HOST_URL + "movies").then(handleHttpErrors);
+  const res = await fetch(MOVIE_URL).then(handleHttpErrors);
   movies = [...res];
   LAST_FETCH.movies = Date.now();
   console.log("movies", movies);
@@ -43,9 +41,7 @@ async function getMovies(): Promise<Array<Movie>> {
   return res;
 }
 
-async function addReservation(newReservation:{}){
-
-}
+async function addReservation(newReservation: {}) {}
 
 export type { Movie };
 
