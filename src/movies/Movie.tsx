f97d2291fc7dd4077e52577f456315d603aca984
Jaes98 <marcus.jaes@gmail.com>
2024-03-18 16:54:50 +0100

    added movie properties

--- src/movies/Movie.tsx
+++ src/movies/Movie.tsx
@@ -20,11 +20,21 @@ export default function Movie() {
             {movie.title} ({movie.id})
           </h3>
           <div style={{ display: "flex" }}>
-            <img style={{ width: 200, margin: 10, flexDirection: "column" }} src={movie.title} alt={movie.title} />
-            <p style={{ display: "inline", flexDirection: "column" }}>{movie.title}</p>
+            <img style={{ width: 200, margin: 10, flexDirection: "column" }} src={movie.poster} alt={movie.poster} />
+            <p style={{ display: "inline", flexDirection: "column" }}>{movie.director}</p>
           </div>
           <hr />
-          <p style={{ whiteSpace: "pre-wrap" }}>{movie.title}</p>
+          <p style={{ whiteSpace: "pre-wrap" }}>{movie.imdbRating}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.actors}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.plot}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.runtime}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.genre}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.rated}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.year}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.website}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.metascore}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.imdbVotes}</p>
+            <p style={{ whiteSpace: "pre-wrap" }}>{movie.response}</p>
         </>
       ) : (
         <h2>Sorry. Movie not found</h2>
