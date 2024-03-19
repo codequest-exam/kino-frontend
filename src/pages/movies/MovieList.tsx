0793075c541f6c373a7d3e5e9f9e4b115c87439c
Jaes98 <marcus.jaes@gmail.com>
2024-03-19 14:53:08 +0100

    MAYBE WORKS??

--- src/pages/movies/MovieList.tsx
+++ src/pages/movies/MovieList.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Movie as APIMovie, getMovies } from "../../services/apiFacade";
-import "./MovieList.css";
+import "./Movielist.css";
 
 export default function MovieList() {
   const [movies, setMovies] = useState<Array<APIMovie>>([]);
