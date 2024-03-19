fb171c71197cbc97b772f27fffd362caa56944d6
LauritsSchou <laurits.schou@gmail.com>
2024-03-19 13:05:24 +0100

    commented out search

--- src/pages/reservations/ReservationLayout.tsx
+++ src/pages/reservations/ReservationLayout.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import AllReservations from './ReservationList';
+import React, { useState } from "react";
+// import AllReservations from "./ReservationList";
 
 const ReservationLayout: React.FC = () => {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(event.target.value);
-    };
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
-    return (
-        <div>
-            <h1>Reservations</h1>
-            <input type="text" placeholder="Search by User" value={searchTerm} onChange={handleSearch} />
-            <AllReservations searchTerm={searchTerm} />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Reservations</h1>
+      <input type="text" placeholder="Search by User" value={searchTerm} onChange={handleSearch} />
+      {/* <AllReservations searchTerm={searchTerm} /> */}
+    </div>
+  );
 };
 
 export default ReservationLayout;
