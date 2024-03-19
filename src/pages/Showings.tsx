45dee2cef1b28286088176dc0162ef81e151555a
LauritsSchou <laurits.schou@gmail.com>
2024-03-19 09:43:55 +0100

    changed showings to show formatted start time and date

--- src/pages/Showings.tsx
+++ src/pages/Showings.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-//import ShowingForm from "../showings/ShowingForm";
 import { Showing, getShowings } from "../services/apiFacade";
 
 export default function Showings() {
@@ -12,11 +11,23 @@ export default function Showings() {
       .catch(() => setError("Error fetching showings, the server might be down."));
   }, []);
 
+  const formatStartTime = (startTime: string) => {
+    const date = new Date(startTime);
+    const formattedStartTime = `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+    return formattedStartTime;
+  };
+  const formatStartDate = (startTime: string) => {
+    const date = new Date(startTime);
+    const formattedStartDate = `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth() + 1).toString().padStart(2, "0")}`;
+    return formattedStartDate;
+  };
+
   const showingTableRows = showings.map((showing) => (
     <tr key={showing.id}>
       <td>{showing.movie.title}</td>
       <td>{showing.hall.roomNumber}</td>
-      <td>{showing.startTime}</td>
+      <td>{formatStartTime(showing.startTime)}</td>
+      <td>{formatStartDate(showing.startTime)}</td>
       <td>{showing.is3d ? "Yes" : "No"}</td>
       <td>{showing.isImax ? "Yes" : "No"}</td>
       <td>{showing.moviePrice}</td>
@@ -36,6 +47,7 @@ export default function Showings() {
             <th>Movie</th>
             <th>Hall number</th>
             <th>Start Time</th>
+            <th>Date</th>
             <th>3D</th>
             <th>IMAX</th>
             <th>Price</th>
