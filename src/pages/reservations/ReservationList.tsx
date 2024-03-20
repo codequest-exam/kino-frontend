02ec2371729b9dab0bfc8cbcecf4f7c2ded25df8
sassnoe <105166994+sassnoe@users.noreply.github.com>
2024-03-20 12:59:26 +0100

    reservations list css
    css for admin reservations list

--- src/pages/reservations/ReservationList.tsx
+++ src/pages/reservations/ReservationList.tsx
@@ -1,41 +1,31 @@
 import { useEffect, useState } from "react";
-import {
-  Reservation as APIReservation,
-  getReservations,
-} from "../../services/apiFacade";
+import { Reservation as APIReservation, getReservations } from "../../services/apiFacade";
 import "./reservations.css";
 
 interface Props {
   searchTerm: string;
 }
 
-const ReservationList: React.FC<Props> = ({searchTerm}) =>{
+const ReservationList: React.FC<Props> = ({ searchTerm }) => {
   const [reservations, setReservations] = useState<Array<APIReservation>>([]);
   console.log("reservations", reservations);
-  
-  const [error, setError] = useState("");
 
+  const [error, setError] = useState("");
 
   useEffect(() => {
     getReservations()
       .then((res) => setReservations(res))
-      .catch(() =>
-        setError("Error fetching reservations, the server might be down.")
-      );
+      .catch(() => setError("Error fetching reservations, the server might be down."));
   }, [searchTerm]);
 
-  const filterReservationsByEmail = reservations.filter(
-    (reservation) =>
-      // reservation.email &&
-      reservation.email.toLowerCase()
-        .includes(searchTerm.toLowerCase()));
+  const filterReservationsByEmail = reservations.filter((reservation) =>
+    // reservation.email &&
+    reservation.email.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   const reservationListItems = filterReservationsByEmail.map((reservation) => {
     const date = new Date(reservation.showing.startTime);
-    const formattedTime = `${date.getHours()}:${date
-      .getMinutes()
-      .toString()
-      .padStart(2, "0")}`;
+    const formattedTime = `${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}`;
     const formattedDate = date.toLocaleString("default", {
       month: "short",
       day: "2-digit",
@@ -43,7 +33,7 @@ const ReservationList: React.FC<Props> = ({searchTerm}) =>{
 
     return (
       <tr key={reservation.id}>
-        <td>{reservation.id}</td>
+        <td className="center-text">{reservation.id}</td>
         <td>{reservation.showing.movie.title}</td>
         <td>
           Kl.{formattedTime} d.{formattedDate}
@@ -51,12 +41,12 @@ const ReservationList: React.FC<Props> = ({searchTerm}) =>{
         <td>{reservation.email}</td>
         <td>UserInfo TBD</td>
         <td>{reservation.price} dkk,-</td>
-        <td>
+        <td className="center-text">
           {reservation.reservedSeats.map((seat) => (
             <span key={seat.id}>{seat.seatNumber},</span>
           ))}
         </td>
-        <td>
+        <td className="center-text">
           {reservation.reservedSeats.map((seat) => (
             <span key={seat.id}>{seat.seatRowNumber}</span>
           ))}
@@ -80,7 +70,7 @@ const ReservationList: React.FC<Props> = ({searchTerm}) =>{
             <th>Email</th>
             <th>User Info</th>
             <th>Price</th>
-            <th>Seat &</th>
+            <th>Seat</th>
             <th>Row</th>
           </tr>
         </thead>
@@ -88,6 +78,6 @@ const ReservationList: React.FC<Props> = ({searchTerm}) =>{
       </table>
     </>
   );
-}
+};
 
 export default ReservationList;
