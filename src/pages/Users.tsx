b807d0bc2786fd82eb197cbeb02b032362175448
LauritsSchou <laurits.schou@gmail.com>
2024-03-20 15:19:14 +0100

    changed import

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { User, editUserRole, getUsers, removeUserRole } from "../services/apiFacade";
+import { editUserRole, getUsers, removeUserRole } from "../services/apiFacade";
+import { User } from "../services/Interfaces";
 import { useNavigate } from "react-router-dom";
 
 export default function Users() {
