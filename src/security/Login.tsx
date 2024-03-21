cc2b8ac01b7a198d91af720c4bab522f690f89f5
LauritsSchou <laurits.schou@gmail.com>
2024-03-21 14:45:52 +0100

    added loading on login screen

--- src/security/Login.tsx
+++ src/security/Login.tsx
@@ -6,6 +6,7 @@ import "./login.css";
 const Login: React.FC = () => {
   const [user, setUser] = useState({ username: "", password: "" });
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -15,11 +16,14 @@ const Login: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setLoading(true);
     try {
       await auth.signIn(user);
       navigate(from, { replace: true });
+      setLoading(false);
     } catch (error) {
       setError("Incorrect username or password");
+      setLoading(false);
     }
   };
 
@@ -30,30 +34,14 @@ const Login: React.FC = () => {
         {error && <div className="error">{error}</div>}
         <div className="login-form-group">
           <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            value={user.username}
-            onChange={(e) =>
-              setUser((prev) => ({ ...prev, username: e.target.value }))
-            }
-            required
-          />
+          <input type="text" name="username" value={user.username} onChange={(e) => setUser((prev) => ({ ...prev, username: e.target.value }))} required />
         </div>
         <div className="login-form-group">
           <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={user.password}
-            onChange={(e) =>
-              setUser((prev) => ({ ...prev, password: e.target.value }))
-            }
-            required
-          />
+          <input type="password" name="password" value={user.password} onChange={(e) => setUser((prev) => ({ ...prev, password: e.target.value }))} required />
         </div>
-        <button type="submit" className="login-btn">
-          Login
+        <button type="submit" className="login-btn" disabled={loading}>
+          {loading ? "Logging in..." : "Login"}
         </button>
       </form>
     </div>
