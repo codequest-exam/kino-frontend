e270ccbf8da933ac50a346adcdb3f81f59c7f313
Jaes98 <marcus.jaes@gmail.com>
2024-03-22 11:58:48 +0100

    Update SeatBookingController.tsx

--- src/pages/SeatBookingController.tsx
+++ src/pages/SeatBookingController.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Checkout from "./Checkout";
 import SeatReservation, { newReservation } from "./SeatReservation";
 
